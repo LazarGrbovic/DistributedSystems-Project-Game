b364134adef37a6a8d2fc55ad39aefd46ace0490
LazarGrbovic <lazarmgrbovic@gmail.com>
2022-01-10 22:08:38 +0100

    Modified GameStatus Class
    * Added DestinationCells Property

--- Game/Game-Logic/Packets/GameStatus.cs
+++ Game/Game-Logic/Packets/GameStatus.cs
@@ -12,11 +12,13 @@ namespace Game_Logic.Packets
         public Cell[] Cells { get; private set; }
 
         public Player[] Players { get; private set; }
+        public DestinationCell[] DestinationCells { get; private set; }
 
-        public GameStatus(Cell[] cells, Player[] players)
+        public GameStatus(Cell[] cells, Player[] players, DestinationCell[] destinationCells)
         {
             this.Cells = cells;
             this.Players = players;
+            this.DestinationCells = destinationCells;
         }
     }
 }
