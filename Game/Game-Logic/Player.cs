73b2ca733ec8e263ecee469ade695e8be10ea5c5
LazarGrbovic <lazarmgrbovic@gmail.com>
2022-01-10 21:35:30 +0100

    Improved Player Class

--- Game/Game-Logic/Player.cs
+++ Game/Game-Logic/Player.cs
@@ -14,9 +14,9 @@ namespace Game_Logic
         public List<FigureEnum> FiguresAtDestination { get; private set; }
         public List<FigureEnum> FiguresAtStart { get; private set; }
 
-        public Player(int id)
+        public Player(PlayerEnum id)
         {
-            this.SetPlayerEnumID(id);
+            this.ID = id;
             this.ActiveFigures = new List<FigureEnum>();
             this.FiguresAtStart = new List<FigureEnum>() { FigureEnum.Figure_1, FigureEnum.Figure_2, FigureEnum.Figure_3, FigureEnum.Figure_4 };
             this.FiguresAtDestination = new List<FigureEnum>();
@@ -32,27 +32,6 @@ namespace Game_Logic
         {
             this.ActiveFigures.Remove(figure);
             this.FiguresAtStart.Add(figure);
-        }
-
-        private void SetPlayerEnumID(int id)
-        {
-            switch (id)
-            {
-                case 1:
-                    this.ID = PlayerEnum.Player_1; 
-                    break;
-                case 2:
-                    this.ID = PlayerEnum.Player_2;
-                    break;
-                case 3:
-                    this.ID = PlayerEnum.Player_3;
-                    break;
-                case 4:
-                    this.ID = PlayerEnum.Player_4;
-                    break;
-                default:
-                    throw new Exception("Invalid ID");
-            }
         }
     }
 }
